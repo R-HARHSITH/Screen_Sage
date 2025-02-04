bd4bbff679778791075f33da08cd2df7ea47643f
R-HARSHITH <harshithrvsb@gmail.com>
2025-02-04 16:52:15 +0530

    changes for playing video

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ if (!fs.existsSync(uploadDir)) {
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: ["http://localhost:5174","https://screen-sage.onrender.com/"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:5174"], credentials: true }));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", req.headers.origin);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
